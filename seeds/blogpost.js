38f9875dd3166601cf3fae494d115cf8de035f5c
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-17 21:21:03 -0500

    added seed titles to test

--- seeds/blogpost.js
+++ seeds/blogpost.js
@@ -2,24 +2,24 @@ const { Post } = require('../models');
 
 const postdata = [
 {
-    title: '',
+    title: 'Why MVC is so important',
     content: '',
     _id: 2,
     user_id: 4,
   },
   {
-    title: '',
+    title: 'Authentication vs. Authorization',
     content: '',
     _id: 2,
     user_id: 4,
   },
   {
-    title: '',
+    title: 'Object Relational Mapping',
     content: '',
     _id: 2,
     user_id: 4,
-  }
+  };
 
-  const seedPost = async () => await Post.bulkCreate(postdata);
+const seedPost = async () => await Post.bulkCreate(postdata);
 
 module.exports = seedPost;

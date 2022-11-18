ef5e385780cca851691f6d49e28cf04ee5229b13
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-17 21:24:19 -0500

    added content to seeds

--- seeds/blogpost.js
+++ seeds/blogpost.js
@@ -3,20 +3,20 @@ const { Post } = require('../models');
 const postdata = [
 {
     title: 'Why MVC is so important',
-    content: '',
+    content: 'MVC allows developers to maintain a true separation of concerns, devising their code between the Model layer for data, the View layer for design and the Controller layer for application login',
     _id: 2,
     user_id: 4,
   },
   {
     title: 'Authentication vs. Authorization',
-    content: '',
+    content: 'There is a difference between authentication and authorization. Authentication means confirming your own identity, whereas authorization means being allowed access to the system.',
     _id: 2,
     user_id: 4,
   },
   {
     title: 'Object Relational Mapping',
-    content: '',
-    _id: 2,
+    content: 'I have really loven learning about ORMs. It has really simplified the way I created queries in SQL!', 
+      _id: 2,
     user_id: 4,
   };
 
